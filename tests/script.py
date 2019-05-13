e55d55db2bfb2185cb1416c67d03f043d1493d9d
technolapin <clement.chomicki@protonmail.fr>
2019-05-13 14:59:24 +0200

    watershed completed

--- tests/script.py
+++ tests/script.py
@@ -10,19 +10,14 @@ def command(cmd):
 
 os.system("cp ../extraction/rangee/00/tranche-0000t00.pgm  seuil.pgm")
 os.system("cp ../extraction/images/test-0.pgm originale.pgm")
+
 command("inverse seuil.pgm seuil_inverse.pgm")
-
 command("dist seuil_inverse.pgm 80 dist.pgm")
-
-command("inverse originale.pgm originale_inverse.pgm")
-command("maxima dist.pgm 4 maxima.pgm")
 command("inverse dist.pgm dist_inv.pgm")
 command("minima dist_inv.pgm 4 minimas.pgm")
+command("watershed dist_inv.pgm minimas.pgm 8 watershed.pgm")
+command("add seuil_inverse.pgm watershed.pgm superpose.pgm")
+os.system("ls")
+command("inverse superpose.pgm final.pgm")
 
-command("seuil dist.pgm 1 dist_seuil.pgm")
-
-print("test")
-command("watershed originale_inverse.pgm dist.pgm 4 watershed.pgm")
-
-command("add seuil.pgm watershed.pgm supperpose.pgm")
 
