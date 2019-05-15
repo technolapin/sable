a3129414a08c41839ef585db4af8862682ff4a02
734F96 <50321866+734F96@users.noreply.github.com>
2019-05-15 10:23:45 +0200

    Modifications

--- gui/environnement_démo_v4.py
+++ gui/environnement_démo_v4.py
@@ -35,29 +35,35 @@ class Graphique3D(FigureCanvasQTAgg) :
     """
     def __init__(self) :
         self.figure = plt.figure( figsize = (7,7) )
-        FigureCanvasQTAgg.__init__(self, self.figure) # Objet de type FigureCanvas
-        self.axes = self.figure.gca(projection = '3d') # On lui dit qu'on veut des axes 3D, et on les stockes dans un attribut
+        FigureCanvasQTAgg.__init__( self, self.figure ) # Objet de type FigureCanvas
+        self.axes = self.figure.gca( projection = '3d' ) # On lui dit qu'on veut des axes 3D, et on les stockes dans un attribut
     
     """
     Dessine ou actualise avec un nouveau graphique
     Le paramètre "liste" est une liste de listes
     Chaque sous-liste représente une courbe
     Ces sous-listes doivent comprendre 3 sous-sous-listes étant les coordonnées X, Y et Z à tracer
+    @param "courbeAfficher" : La courbe à afficher dans "liste" + 1, 0 si il faut les afficher toutes
+    @param "tempsAfficher" : L'instant à afficher dans "liste" + 1, 0 si il faut afficher tous les instants
     """
-    def dessinerGraphique3D(self, liste) : # Procédure qui dessine le graphique
+    def dessinerGraphique3D(self, liste, courbeAfficher, tempsAfficher) : # Procédure qui dessine le graphique
         self.axes.clear() # Nettoie les axes
-        self.axes.set_xlabel('Axe X') # Label sur l'axe X
-        self.axes.set_ylabel('Axe Y') # Label sur l'axe Y
-        self.axes.set_zlabel('Axe Z') # Label sur l'axe Z
-        self.axes.set_aspect('equal') # Permet d'avoir un repère orthonormal
-        for courbe in liste :
-            couleur = choice(["b", "g", "r", "c", "m", "y"]) # Choisi aléatoirement dans la liste des couleurs de base de Matplotlib
-            self.axes.plot(courbe[0], courbe[1], courbe[2], couleur + 'o-') # Dessine le graphique 3D à partir de 3 listes dans les axes
+        self.axes.set_xlabel( 'Axe X' ) # Label sur l'axe X
+        self.axes.set_ylabel( 'Axe Y' ) # Label sur l'axe Y
+        self.axes.set_zlabel( 'Axe Z' ) # Label sur l'axe Z
+        self.axes.set_aspect( 'equal' ) # Permet d'avoir un repère orthonormal
+        if courbeAfficher != 0 :
+            couleur = choice( ["b", "g", "r", "c", "m", "y"] ) # Choisi aléatoirement dans la liste des couleurs de base de Matplotlib
+            self.axes.plot( liste[courbeAfficher - 1][0], liste[courbeAfficher - 1][1], liste[courbeAfficher - 1][2], couleur + 'o-' ) # Dessine le graphique 3D à partir de 3 listes dans les axes
+        else :
+            for courbe in liste :
+                couleur = choice( ["b", "g", "r", "c", "m", "y"] ) # Choisi aléatoirement dans la liste des couleurs de base de Matplotlib
+                self.axes.plot( courbe[0], courbe[1], courbe[2], couleur + 'o-' ) # Dessine le graphique 3D à partir de 3 listes dans les axes
         self.draw() # Dessine le graphique 3D avec les axes
 
 """
-Classe Fenetre, hérite de la classe QTabWidget (Et plus QWidget)
-Cette classe permet de gérer la fenêtre Qt
+Classe Fenetre, hérite de la classe QTabWidget (Et plus QWidget vu qu'on veut faire des onglets)
+Cette classe permet de gérer la fenêtre Qt avec onglets (Appel de la procédure "addTab()")
 Le graphique 3D est inséré dedans
 """
 class Fenetre(QTabWidget) :
@@ -68,7 +74,7 @@ class Fenetre(QTabWidget) :
         super(Fenetre, self).__init__(parent) # Appel du constructeur de QWidget
         
         # Taille minimale de la fenêtre, en pixels
-        self.setMinimumSize(QSize(400, 400));
+        self.setMinimumSize( QSize(400, 400) );
         
         # Création des onglets de la fenêtre
         self.onglet1 = QWidget()
@@ -76,9 +82,9 @@ class Fenetre(QTabWidget) :
         self.onglet3 = QWidget()  
         
         # Ajout des onglets à la fenêtre  	    
-        self.addTab(self.onglet1, "Visualisation du Graphique") 
-        self.addTab(self.onglet2, "Onglet 2")
-        self.addTab(self.onglet3, "Onglet 3")
+        self.addTab( self.onglet1, "Visualisation du Graphique" ) 
+        self.addTab( self.onglet2, "Onglet 2" )
+        self.addTab( self.onglet3, "Onglet 3" )
         
         # Appel des procédures qui remplissent les onglets
         self.tabGraphique3D()
@@ -100,27 +106,29 @@ class Fenetre(QTabWidget) :
 #       Devenu inutile car on définit la taille minimale de la fenêtre
         
         # Défilement temporel
-        self.barreDeScroll1 = QScrollBar() # C'est une barre de défilement
-        self.barreDeScroll1.setMaximum( len(graphe[0][0]) ) # Défini le nombre de valeurs qu'on peut y parcourir
-        self.barreDeScroll1.sliderMoved.connect(self.changementGraphique3D) # La procédure à appeler lorsque l'utilisateur y touche
+        self.barreDeScrollCourbes = QScrollBar() # C'est une barre de défilement
+        self.barreDeScrollCourbes.setMaximum( len(graphe[0]) + 1 ) # Défini le nombre de valeurs qu'on peut y parcourir
+        # len(graphe[0][0]) est le nombre de courbes
+        self.barreDeScrollCourbes.sliderMoved.connect( self.changementGraphique3D ) # La procédure à appeler lorsque l'utilisateur y touche
         
         # Défilement coupes
-        self.barreDeScroll2 = QScrollBar()
-        self.barreDeScroll2.setMaximum(255)
-        self.barreDeScroll2.sliderMoved.connect(self.changementGraphique3D)
+        self.barreDeScrollTemps = QScrollBar()
+        self.barreDeScrollTemps.setMaximum( len(graphe[0][0]) + 1 )
+        # len(graphe[0][0]) est le nombre d'échantillons temporels dont on dispose
+        self.barreDeScrollTemps.sliderMoved.connect( self.changementGraphique3D )
         
         grille = QGridLayout() # C'est l'intérieur de la fenêtre, une grille ("layout")
         
-#        grille.addWidget(self.menuSelection, 1, 1) # Ajoute le menu déroulant en position ligne 2 colonne 1
-        grille.addWidget(self.graphique3D, 2, 1) # Ajoute le graphique 3D en position ligne 2 colonne 1
-        grille.addWidget(self.barreDeScroll1, 2, 2) # Ajoute la barre de défilement 1 en position ligne 2 colonne 2
-        grille.addWidget(self.barreDeScroll2, 2, 3) # Ajoute la barre de défilement 2 en position ligne 2 colonne 2
+#        grille.addWidget( self.menuSelection, 1, 1 ) # Ajoute le menu déroulant en position ligne 2 colonne 1
+        grille.addWidget( self.graphique3D, 2, 1 ) # Ajoute le graphique 3D en position ligne 2 colonne 1
+        grille.addWidget( self.barreDeScrollCourbes, 2, 2 ) # Ajoute la barre de défilement 1 en position ligne 2 colonne 2
+        grille.addWidget( self.barreDeScrollTemps, 2, 3 ) # Ajoute la barre de défilement 2 en position ligne 2 colonne 2
         
         self.setLayout(grille) # Definit notre grille comme grille à utiliser
         
-        self.graphique3D.dessinerGraphique3D( graphe ) # Afficher graphique de base
+        self.graphique3D.dessinerGraphique3D( graphe, -1, -1 ) # Afficher graphique de base
         
-        self.onglet1.setLayout(grille) # Ajout du contenu dans l'onglet
+        self.onglet1.setLayout( grille ) # Ajout du contenu dans l'onglet
     
     """
     Onglet 2
@@ -128,7 +136,7 @@ class Fenetre(QTabWidget) :
     def tabOnglet2(self) :
         grille = QGridLayout()
         
-        self.onglet2.setLayout(grille)
+        self.onglet2.setLayout( grille )
     
     """
     Onglet 3
@@ -136,7 +144,7 @@ class Fenetre(QTabWidget) :
     def tabOnglet3(self) :
         grille = QGridLayout()
         
-        self.onglet3.setLayout(grille)
+        self.onglet3.setLayout( grille )
     
     """
     Gère les changements par l'utilisateur dans le menu déroulant de sélection
@@ -148,7 +156,9 @@ class Fenetre(QTabWidget) :
 #        if graphiqueDemande == "Graph2" :
 #            self.graph2()
 #       Devenu inutlie puisqu'on a supprimé les menus déroulants
-         self.barreDeScroll1.value()
+         self.graphique3D.dessinerGraphique3D( graphe, self.barreDeScrollCourbes.value(), self.barreDeScrollTemps.value() )
+         
+         print( "[Debug] Temps : " + str( self.barreDeScrollCourbes.value() ) + ", Courbe : " + str( self.barreDeScrollTemps.value() ) )
 
 
 """
