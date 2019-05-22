a13394138d3e9997b386e5e0d29e3627410c7b10
technolapin <clement.chomicki@protonmail.fr>
2019-05-22 16:50:22 +0200

    tests sur les images 3D

--- tests/script.py
+++ tests/script.py
@@ -1,23 +1,13 @@
 import os
-os.system("ls")
-root = ".."
-libs = root + "/pink/linux/bin/"
-
-os.system("rm *.pgm")
 
 def command(cmd):
-    os.system(libs+cmd)
+    os.system("../pink/linux/bin/"+cmd)
 
-os.system("cp ../extraction/rangee/00/tranche-0000t00.pgm  seuil.pgm")
-os.system("cp ../extraction/images/test-0.pgm originale.pgm")
 
-command("inverse seuil.pgm seuil_inverse.pgm")
-command("dist seuil_inverse.pgm 80 dist.pgm")
-command("inverse dist.pgm dist_inv.pgm")
+command("seuil image_3D_t00.pgm 170 seuil.pgm")
+command("inverse seuil.pgm seuil_inv.pgm")
+command("dist seuil_inv.pgm 80 dist.pgm")
+command("seuil dist.pgm 3 dist_seuil.pgm")
+command("inverse dist_seuil.pgm dist_inv.pgm")
 command("minima dist_inv.pgm 4 minimas.pgm")
-command("watershed dist_inv.pgm minimas.pgm 8 watershed.pgm")
-command("add seuil_inverse.pgm watershed.pgm superpose.pgm")
-os.system("ls")
-command("inverse superpose.pgm final.pgm")
-
-
+command("pgm2vtk minimas.pgm 1 test.vtk")
