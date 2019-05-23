2730057b3138813694fb2a713eb24e48aa4e9156
Neoxes <46921161+Neoxes38@users.noreply.github.com>
2019-05-23 10:52:33 +0200

    Yo! (2)

--- gui/tests_Alexandre/Affiche_une_image (test (2)).py
+++ gui/tests_Alexandre/Affiche_une_image (test (2)).py
@@ -15,9 +15,9 @@ class ImageViewer(object):
     def setupUi(self, Viewer): 
         Viewer.resize(640, 480) 
         Viewer.setWindowTitle(u"Test de création de boutons") 	
-        self.image_1 = "Test1.pgm" 
-        self.image_2 = "Image_btn_g"	
-        self.image_3 = "Image_btn_d"
+        self.image_1 = "Test1.pgm"       # Image "principale"
+        self.image_2 = "Image_btn_g"	# Icone du bouton de gauche
+        self.image_3 = "Image_btn_d"     # Icone du couton de droite
         self.centralwidget    = QWidget(Viewer) 
         self.gridLayout       = QGridLayout(self.centralwidget) 
         self.verticalLayout_2 = QVBoxLayout() 
@@ -27,16 +27,14 @@ class ImageViewer(object):
         
         # QLabel 
         self.label = QLabel(self.centralwidget) 
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, 
-                                		QSizePolicy.Fixed) 
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed) 
         self.label.setSizePolicy(sizePolicy) 
         self.label.setPixmap(QPixmap(self.image_1)) 
         self.label.setScaledContents(False) 
         self.horizontalLayout.addWidget(self.label)         
         
         self.verticalLayout = QVBoxLayout() 
-        spacerItem = QSpacerItem(20, 18, QSizePolicy.Minimum, 
-        	                        	QSizePolicy.Fixed) 
+        spacerItem = QSpacerItem(20, 18, QSizePolicy.Minimum, QSizePolicy.Fixed) 
         self.verticalLayout.addItem(spacerItem) 
         self.horizontalLayout.addLayout(self.verticalLayout) 
         self.verticalLayout_2.addLayout(self.horizontalLayout) 
@@ -45,40 +43,42 @@ class ImageViewer(object):
 
         
         # QPushButton 
+        
+        # Bouton 1
         self.pushButton1 = QPushButton(self.centralwidget) 
         self.pushButton1.setText("Image précédente") 
+       
+        icon1 = QIcon() 
+        icon1.addPixmap(QPixmap(self.image_2),QIcon.Normal, QIcon.Off)
         
+        self.pushButton1.setIcon(icon1) 
+        
+        # Boutno 2    
+    
         self.pushButton2 = QPushButton(self.centralwidget) 
         self.pushButton2.setText("Image suivante") 
         
-        icon1 = QIcon() 
-        icon1.addPixmap(QPixmap(self.image_2),QIcon.Normal, 
-                                        QIcon.Off)
         icon2 = QIcon() 
-        icon2.addPixmap(QPixmap(self.image_3),QIcon.Normal, 
-                                        QIcon.Off)
-        
-        self.pushButton1.setIcon(icon1) 
-        self.horizontalLayout_2.addWidget(self.pushButton1) 
+        icon2.addPixmap(QPixmap(self.image_3),QIcon.Normal, QIcon.Off)
         
         self.pushButton2.setIcon(icon2) 
-        self.horizontalLayout_2.addWidget(self.pushButton2) 
-        
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout_2)         
+        # Ajout des boutons au Layout
+
+        self.horizontalLayout_2.addWidget(self.pushButton1) 
+        self.horizontalLayout_2.addWidget(self.pushButton2) 
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)                 
         
-        
+        # Positionnement des boutons grâce à des QSpacerItem
         
         self.horizontalLayout_3 = QHBoxLayout() 
-        spacerItem1 = QSpacerItem(40, 20, QSizePolicy.Expanding, 
-                                    	QSizePolicy.Minimum) 
+        spacerItem1 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum) 
         self.horizontalLayout_3.addItem(spacerItem1) 
         self.verticalLayout_2.addLayout(self.horizontalLayout_3)
         
         
         self.horizontalLayout_4 = QHBoxLayout() 
-        spacerItem2 = QSpacerItem(40, 20, QSizePolicy.Expanding, 
-                                    	QSizePolicy.Minimum) 
+        spacerItem2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum) 
         self.horizontalLayout_4.addItem(spacerItem2) 
         self.verticalLayout_2.addLayout(self.horizontalLayout_4)
         
@@ -86,29 +86,10 @@ class ImageViewer(object):
         self.gridLayout.addLayout(self.verticalLayout_2, 0, 0, 1, 1) 
         Viewer.setCentralWidget(self.centralwidget) 
         
-
-        
-    def update_push_button1(self, idx): 
-        if not idx: 
-            return 
-        if idx == 1:
-            self.pushButton1.setAutoDefault(not self.pushButton1.autoDefault()) 
-        elif idx == 2: 
-            self.pushButton1.setDefault(not self.pushButton1.isDefault()) 
-        else: 
-            self.pushButton1.setFlat(not self.pushButton1.isFlat()) 
         
         
-        
-    def update_push_button2(self, idx): 
-        if not idx: 
-            return 
-        if idx == 1:
-            self.pushButton2.setAutoDefault(not self.pushButton2.autoDefault()) 
-        elif idx == 2: 
-            self.pushButton2.setDefault(not self.pushButton2.isDefault()) 
-        else: 
-            self.pushButton2.setFlat(not self.pushButton2.isFlat()) 
+    def je_sers_a_rien_mais_ca_fonctionne_pas_sans_moi(self, idx): 
+        return 
         
         
         
