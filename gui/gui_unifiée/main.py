5d3b8e55733f9cb759db5b0f55b3e72b5c4e0197
734F96 <50321866+734F96@users.noreply.github.com>
2019-06-07 09:53:19 +0200

    Choix ouvrir ou lancer traitement

--- gui/gui_unifiée/main.py
+++ gui/gui_unifiée/main.py
@@ -10,9 +10,11 @@ from parametres_pour_demo import grapheDeDemonstration
 
 """
 Fonction de validation du fichier demandé
+@param fichier : Fichier choisi par l'utilisateur
+@param lancer : True pour lancer un traitement, False pour ouvrir un traitement
 """
 # TODO : A améliorer !
-def validationFichier( fichier ) :
+def validationFichier( fichier, lancer ) :
     if fichier == "" :
         return False
     return True
@@ -20,9 +22,33 @@ def validationFichier( fichier ) :
 
 """
 Fonction de traitement d'image (Appel tout le travail de traitement d'image)
+@param fichier : Fichier choisi par l'utilisateur
 """
 def traitementImage( fichier ) :
     return
+
+
+"""
+Demande à l'utilisateur un fichier pour lancer un traitement ou ouvrir un fichier de traitement
+@param lancer : True pour lancer un traitement, False pour ouvrir un traitement
+"""
+def lancerOuOuvrirTraitement( lancer ) :
+    fileDialog = QFileDialog() # Crée un objet de type QFileDialog (Fenêtre pour choisir un fichier)
+    if lancer : fileDialog.setWindowTitle("Veuillez choisir le fichier TIFF") # Définit le nom de la fenêtre
+    else : fileDialog.setWindowTitle("Veuillez choisir le fichier")
+    fichierDemande = fileDialog.getOpenFileName()[0] # Permet aussi d'attendre qu'il y ait un fichier demandé
+    print( "[Debug] Fichier demandé : " + fichierDemande )
+    fileDialog.close() # Fermer la fenêtre
+    
+    if not validationFichier( fichierDemande, lancer ) : # Si la validation de ce fichier échoue
+        QMessageBox.about(None, "Information", "Ce fichier est invalide !")
+    else :
+        traitementImage( fichierDemande )
+        
+        fenetre = Fenetre( grapheDeDemonstration ) # Crée un objet de type Fenetre
+        fenetre.setWindowTitle("Graphique 3D (DÉMONSTRATION)") # Définit le nom de la fenêtre
+        fenetre.show() # Affiche la fenêtre
+        application.exec_() # Attendre que tout ce qui est en cours soit exécuté
 
 
 """
@@ -34,18 +60,27 @@ Code principal
 if __name__ == '__main__' :
     application = QApplication(sys.argv) # Crée un objet de type QApplication (Doit être fait avant la fenêtre)
     
-    fileDialog = QFileDialog() # Crée un objet de type QFileDialog (Fenêtre pour choisir un fichier)
-    fileDialog.setWindowTitle("Veuillez choisir le fichier TIFF") # Définit le nom de la fenêtre
-    fichierDemande = fileDialog.getOpenFileName()[0] # Permet aussi d'attendre qu'il y ait un fichier demandé
-    print( "[Debug] Fichier demandé : " + fichierDemande )
-    fileDialog.close() # Fermer la fenêtre
+    msgBox = QMessageBox()
+    msgBox.setText("Voulez-vous lancer un nouveau traitement ?")
+    msgBox.setWindowTitle("Information")
+    msgBox.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+    returnValue = msgBox.exec()
     
-    if not validationFichier( fichierDemande ) : # Si la validation de ce fichier échoue
-        QMessageBox.about(None, "Information", "Ce fichier est invalide !")
-    else :
-        traitementImage( fichierDemande )
+    if returnValue == QMessageBox.Yes :
+        print( "[Debug] L'utilisateur veut lancer un nouveau traitement." )
+        lancerOuOuvrirTraitement( True )
         
-        fenetre = Fenetre( grapheDeDemonstration ) # Crée un objet de type Fenetre
-        fenetre.setWindowTitle("Graphique 3D (DÉMONSTRATION)") # Définit le nom de la fenêtre
-        fenetre.show() # Affiche la fenêtre
-        application.exec_() # Attendre que tout ce qui est en cours soit exécuté
+    else :
+        print( "[Debug] L'utilisateur ne veut pas lancer un nouveau traitement" )
+        msgBox = QMessageBox()
+        msgBox.setText("Voulez-vous ouvrir un traitement déjà effectué ?")
+        msgBox.setWindowTitle("Information")
+        msgBox.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        returnValue = msgBox.exec()
+        
+        if returnValue == QMessageBox.Yes :
+            print( "[Debug] L'utilisateur veut ouvrir un traitement déjà effectué." )
+            lancerOuOuvrirTraitement( False )
+        
+        else :
+            QMessageBox.about(None, "Information", "Au revoir !")
