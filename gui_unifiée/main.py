e2ee94b3a969b23bb43aa14c6f413f304fdea58a
734F96 <50321866+734F96@users.noreply.github.com>
2019-06-14 10:54:49 +0200

    Améliorations

--- gui_unifiée/main.py
+++ gui_unifiée/main.py
@@ -22,14 +22,15 @@ msgBox.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
 returnValue = msgBox.exec()
 
 if returnValue == QMessageBox.Yes :
-    print( "[Debug] L'utilisateur veut lancer un nouveau traitement." )
+    print( "[Info Main] L'utilisateur veut lancer un nouveau traitement." )
     if platform.system() != "Linux" :
+        print( "[Info Main] Un système Unix est nécéssaire pour lancer un traitement." )
         QMessageBox.about(None, "Information", "Vous devez exécuter ce logiciel sur un système Linux pour pouvoir réaliser un traitement !\nEn effet, notre système de traitement est compilé uniquement pour cette plateforme.")
     else :
         lancerOuOuvrirTraitement( True, application )
     
 else :
-    print( "[Debug] L'utilisateur ne veut pas lancer un nouveau traitement" )
+    print( "[Info Main] L'utilisateur ne veut pas lancer un nouveau traitement" )
     msgBox = QMessageBox()
     msgBox.setText("Voulez-vous ouvrir un traitement déjà effectué ?")
     msgBox.setWindowTitle("Information")
@@ -37,9 +38,9 @@ else :
     returnValue = msgBox.exec()
     
     if returnValue == QMessageBox.Yes :
-        print( "[Debug] L'utilisateur veut ouvrir un traitement déjà effectué." )
+        print( "[Info Main] L'utilisateur veut ouvrir un traitement déjà effectué." )
         lancerOuOuvrirTraitement( False, application )
     
     else :
-        print( "[Debug] L'utilisateur ne veut pas ouvrir un traitement déjà effectué." )
+        print( "[Info Main] L'utilisateur ne veut pas ouvrir un traitement déjà effectué." )
         QMessageBox.about(None, "Information", "Au revoir !")
