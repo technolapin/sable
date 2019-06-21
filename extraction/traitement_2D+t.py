44c5a6fae216e48f33597d3b1ff22e3c8c2b07bc
barbaylis <maylis.montani.gmail.com>
2019-06-21 15:25:17 +0200

    recreation des vtk avec mcube pour le 2D+t

--- extraction/traitement_2D+t.py
+++ extraction/traitement_2D+t.py
@@ -49,5 +49,5 @@ for i in range(0,n_coupe):
     ligne="catpgm images_process_2D+t/image_process_coupe_"+numerote(j,3)+"_t_ "+ str(0)+ " "+str(15)+" [80 80 16] images_2D+t/image_2D+t_coupe_"+numerote(i,3)+".pgm"
     command(ligne)
     command("seuil images_2D+t/image_2D+t_coupe_"+numerote(i,3)+".pgm "+str(seuil)+" images_2D+t/image_2D+t_s_coupe_"+numerote(i,3)+".pgm")
-    command("pgm2vtk images_2D+t/image_2D+t_s_coupe_"+numerote(i,3)+".pgm 2 images_2D+t_visu/image_2D+t_visu_coupe_"+numerote(i,3)+".vtk")
+    command("mcube images_2D+t/image_2D+t_s_coupe_"+numerote(i,3)+".pgm 0 5 0 VTK images_2D+t_visu/image_2D+t_visu_coupe_"+numerote(i,3)+".vtk")
     
